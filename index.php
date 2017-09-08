45e64ac844449b7f4b611f1c4fcf83de3bf00f8a
Chris Hurst <chris@hurst.cc>
2017-09-08 12:30:01 -0500

    initial CV files

---
+++ index.php
@@ -0,0 +1,197 @@
+<html class="notranslate" lang="en">
+<head>
+    <meta name="theme-color" content="#ffffff">
+    <title>Chris Hurst</title>
+    <meta name="description" content="Chris Hurst">
+    <meta charset="UTF-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link rel="stylesheet" media="all" href="assets/fonts/font.css">
+<!--     <link rel="stylesheet" media="all" href="assets/application-eb97178f5c7ec5b5cb8b15964ff2ee8eee9bfa4a0e809df21d0308e8f2902167.css"> -->
+    <script src="assets/application-d66b520b7003c750122b9de08ccf421defe0b2d30b398dfda6186ba734e39ef0.js"></script>
+    <!--[if lt IE 9]><script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if IE]><script src="assets/excanvas.compiled-37424e0cbcaa0e11b37fafd4a988700c395e7d4cf3e823ac9db8167c76f25a7d.js"></script><![endif]-->
+
+    <link rel="stylesheet" media="all" href="assets/main-df2c9ee8d3dda659b801df27d933761e58eee5e273743218eb96f052f0b15ef7.css" data-turbolinks-track="true" />
+    <script src="assets/my_contentbuilder-221537b2f72f51ebee0c870fa5063b80d15841597ffef0db6ddc97e66204bdfc.js" data-turbolinks-track="true"></script>
+    <style type="text/css">
+    var {
+        font-style: inherit;
+    }
+
+    ljs {
+        border: 1px solid rgba(0, 0, 0, 0.5);
+    }
+
+    ljs,
+    ljs * {
+        cursor: pointer;
+    }
+
+    ljs:hover {
+        background-color: rgba(255, 255, 255, 0.1)
+    }
+    .container-main { margin-top: 30px; }
+    p, .contentarea p { font-size: 14px !important; line-height: 1.3em;
+        font-family: "Open Sans", "Microsoft JhengHei", sans-serif;
+        font-weight: 400;
+        letter-spacing: 0px; }
+    h1 { font-size: 22px; margin-bottom: 5px; }
+    i.arr { color: #13ab67; font-weight: bold; }
+    i.arr::after { content: ' '; font-weight: bold; padding-right: 6px; }
+    .snippet-features-003 .col-xs-12 { margin-top: 15px; }
+    .lessmargin { margin-bottom: 10px !important; }
+    .item-bullet {padding-left: 0 !important; }
+    .snippet-experiences-013 { margin-left: -45px !important; }
+    .snippet-experiences-013 .item { margin-left: -30px !important; }
+    /*.snippet-experiences-013 .row { padding-left: 50px !important; }*/
+    </style>
+    <meta name="google" content="notranslate">
+</head>
+
+<body id="items-draft">
+
+    <div class="container-main container">
+        <div id="alerts"></div>
+        <div id="global-status-container">
+            <div id="global-status"></div>
+        </div>
+        <div class="contentarea" id="item" notranslate="">
+                <div class="row snippet-profile-071 lessmargin" style="text-align: left;">
+                    <div class="col-sm-12">
+                        <h1><b style="font-size: 32px; color: #4A4A4A;">Chris Hurst</b></h1>
+                        <p>I'm a full-stack web developer focused on PHP eCommerce frameworks. I've worked with a wide array of web technologies, languages, and services over the last 10 years and I'm excited to join a team of talented and friendly professionals who share my passion for developing innovative web applications.</p>
+                    </div>
+                </div>
+                <div class="row snippet-features-003">
+                    <div class="col-sm-12">
+                        <div class="row" title="">
+                            <div class="col-xs-12 col-sm-6 item">
+                                <p style="white-space: nowrap;"><span style="color: rgb(136, 136, 136);"><i class="fa size-12"></i>&nbsp;</span>&nbsp;<a href="mailto:hurstc@gmail.com" target="_blank" style="font-size: 16px; background-color: rgb(255, 255, 255);" rel="nofollow noopener noreferrer"><b style="color: #13ab67;">hurstc@gmail.com</b></a></p>
+                                <p style="white-space: nowrap;"><span style="color: rgb(136, 136, 136);"><i class="fa"></i>&nbsp;</span> (316) 204-6704</p>
+                            </div>
+                            <div class="col-xs-12 col-sm-6 item">
+                                <p style="white-space: nowrap;"><i class="fa" style="color: rgb(136, 136, 136);"></i>&nbsp; <a href="http://www.linkedin.com/in/hurstc" target="_blank" rel="nofollow noopener noreferrer"><b style="font-size: 16px;">linkedin.com/in/hurstc</b></a></p>
+                                <p style="white-space: nowrap;"><span style="color: #13ab67;"><i class="fa" onclick="return window.location.replace('https://goo.gl/maps/vs3TTP87tbJ2');"></i>&nbsp;</span> &nbsp;Austin, TX, USA</p>
+                            </div>
+                            <!-- <div class="col-xs-4 item"></div> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="row snippet-experiences-014">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-12" >
+                                    <h1 style="margin-bottom: 15px;"><b style="color: #4A4A4A; font-size: 32px; margin-bottom: 35px;">Skills</b></h1>
+                                </div>
+                        </div>
+
+                        <div class="row" data-original-title="" title="">
+                            <div class="col-sm-6 item" >
+                                <p><i class="fa arr"></i>9+ years LAMP (Linux, Apache, MySQL, PHP) development experience</p>
+                                <p><i class="fa arr"></i>Mastery of MVC frameworks (Wordpress, Magento, Laravel, Zend)</p>
+                                <p><i class="fa arr"></i>9+ years JavaScript development experience</p>
+                                <p><i class="fa arr"></i>Strength with JQuery and other frameworks, Bootstrap, Foundation</p>
+                                <p><i class="fa arr"></i>Create web interfaces, widgets, rich clients and other front-end<br /><span style="padding-left: 12px">components (HTML5,CSS3,JavaScript)</span></p>
+                                <p><i class="fa arr"></i>Expert understanding of server side app development and<br /><span style="padding-left: 12px">scalable web-services, especially REST, JSON, and XML.</span></p>
+                                <p><i class="fa arr"></i>Intimate knowledge of eCommerce checkout flow, web<br /><span style="padding-left: 12px">service integration, and customer / product / inventory management</span></p>
+                                <p><i class="fa arr"></i>MySQL administration, setup, security, backups, &amp; user privileges</p>
+                            </div>
+                            <div class="col-sm-6 item">
+                                <p><i class="fa arr"></i>Experience in designing and developing solutions by effectively<br /><span style="padding-left: 12px">working directly with the client</span></p>
+                                <p><i class="fa arr"></i>Follow-through from project inception to detailed completion</p>
+                                <p><i class="fa arr"></i>Provide input for build requirements, design process, &amp; LOEs</p>
+                                <p><i class="fa arr"></i>Proficient with Linux CLI, SSH, AWS, and remote server management</p>
+                                <p><i class="fa arr"></i>Expert knowledge of modern security methods &amp; best practices<br /><span style="padding-left: 12px">(SQL injection, cross-site scripting, proper data storage practices)</span></p>
+                                <p><i class="fa arr"></i>Test driven development and continuous integration</p>
+                                <p><i class="fa arr"></i>Knowledge of advanced Git techniques and workflows</p>
+                                <p><i class="fa arr"></i>Excellent written and verbal communication skills</p>
+                                <p><i class="fa arr"></i>Attempt to learn, grow, and master new skills every day</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row snippet-experiences-013">
+                    <div class="col-xs-12" style="">
+                        <div class="row" style="margin-left: 43px;">
+                            <div class="col-xs-12 item" style="margin-bottom: 5px;">
+                                <h1><b style="color: #4A4A4A; font-size: 32px; ">Experience</b></h1>
+                            </div>
+                        </div>
+                        <div class="row" title="">
+                            <div class="col-xs-2 col-sm-1 item-bullet"></div>
+                            <div class="col-xs-10 col-sm-11 item">
+                                <h3><b>Wide Eyed Media</b>, <span style="color: #4A4A4A;">2007 - Present</span></h3>
+                                <p><span style="font-size: 14px;">Developed and maintained websites, applications, modules, and features based on client requirements.</span></p>
+                                <p><span style="font-size: 14px;">Managed work tickets and delivered code according to project deadlines</span></p>
+                                <p><span style="font-size: 14px;">Wrote estimates (LOEs) and kept track of time logged for each project ticket.<br>Worked extensively with Magento and Wordpress, including advanced and highly technical module development.</span></p>
+                                <p><span style="font-size: 14px;">Setup and maintained JavaScript for analytics, form submission, image gallery / slideshow, cart and checkout.</span></p>
+                                <p><span style="font-size: 14px;">Typically, these actions were taken for each new client project:<br></span></p>
+                                <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">
+                                    <p><i class="fa arr"></i>Answered questions from client about design limitations, features, and provided LOEs</p>
+                                    <p><i class="fa arr"></i>Setup a new web server (Typically Linux/Apache/PHP) and installed the platform chosen by client</p>
+                                    <p><i class="fa arr"></i>Using the approved graphics / mockup, created all required HTML/CSS/XML/JS theme files used by the platform</p>
+                                    <p><i class="fa arr"></i>Created modules as needed for any custom features</p>
+                                    <p><i class="fa arr"></i>Typical modules included: advanced search/product features, order fulfillment, payment gateways, custom menus</p>
+                                    <p><i class="fa arr"></i>Created an integration process for importing and updating product/customer/inventory data from an external source</p>
+                                    <p><i class="fa arr"></i>Tested, debugged, and commented changes before staging them for approval.</p>
+                                </blockquote>
+                                <div>
+                                    <div><span style="font-size: 14px;">Apps &amp; Utilities Used Daily:</span></div>
+                                </div>
+                                <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">
+                                    <p><i class="fa arr"></i>SSH, FileZilla, SublimeText3, Chrome Dev. Tools, GIT, LESS, Composer</p>
+                                    <p><i class="fa arr"></i>REST/SOAP/XML/JSON</p>
+                                    <p><i class="fa arr"></i>Stripe, Authorize.net, or Paypal integration</p>
+                                    <p><i class="fa arr"></i>jQuery, AJAX calls, and async. behavior</p>
+                                    <p><i class="fa arr"></i>Github, Assembla, Trello, Slack</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div class="row" title="">
+                            <div class="col-xs-2 col-sm-1 item-bullet"></div>
+                            <div class="col-xs-10 col-sm-11 item" style="margin-bottom: 0px;">
+                                <h3><b>Eden's Botanicals</b>, <span style="color: #4A4A4A;">2011 - 2015</span></h3>
+                                <p><span style="font-size: 14px;">Wrote a series of custom payment gateway modules for the eCommerce platform Interspire (now called BigCommerce)</span></p>
+                                <div>
+                                    <blockquote style="margin: 0 0 0 40px; border: none; padding: 0px;">
+                                        <p><i class="fa arr"></i>Written in PHP, with template files consisting of HTML/CSS/JS</p>
+                                        <p><i class="fa arr"></i>Used existing configuration hooks for getting and setting gateway credentials.</p>
+                                        <p><i class="fa arr"></i>Determined the web service architecture and response format used by the gateway. (HTTP,SOAP,REST, XML/JSON/Text)</p>
+                                        <p><i class="fa arr"></i>Implemented the interface in PHP and used it to send checkout data to the gateway and interpret the response correctly.</p>
+                                    </blockquote><span style="font-size: 14px;">A different module allowed for switching the active gateway automatically after a daily payment limit was reached.</span>
+                                    <p></p>
+                                    <p><span style="font-size: 14px;">Another allowed for specified products to carry additional checkout requirements (Must agree to product terms, age restriction, etc)</span></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" title="">
+                            <div class="col-xs-2 col-sm-1 item-bullet"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row snippet-education" style="margin-top: 10px; margin-bottom: 5px;">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-12">
+                               <h1 style=" margin-bottom: 15px;"><b style="color: #4A4A4A; font-size: 32px; ">Education</b></h1>
+                            </div>
+                        </div>
+                        <div class="row item">
+                            <div class="col-xs-12">
+
+                                <p><i class="fa arr"></i>Computer Science Engineering @ University of Kansas (42 hours completed,16 remaining)</p>
+                                <p><i class="fa arr"></i>National Merit Scholar Commendation (2005)</p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</body>
+
+</html>
